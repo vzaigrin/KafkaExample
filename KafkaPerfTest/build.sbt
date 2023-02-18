5f83b292c560cf7e4abddbd076c99cbca581cc8c
Vadim Zaigrin <vzaigrin@yandex.ru>
2023-02-18 21:53:52 +0300

    Update KafkaPerfTest example

--- KafkaPerfTest/build.sbt
+++ KafkaPerfTest/build.sbt
@@ -1,9 +1,9 @@
 ThisBuild / organization := "ru.example"
 ThisBuild / version := "1.0"
-ThisBuild / scalaVersion := "2.13.4"
+ThisBuild / scalaVersion := "2.13.10"
 
-lazy val kafkaVersion   = "3.0.0"
-lazy val logbackVersion = "1.2.10"
+lazy val kafkaVersion   = "3.4.0"
+lazy val logbackVersion = "1.3.0"
 
 ThisBuild / libraryDependencies ++= Seq(
   "org.apache.kafka" % "kafka-clients"   % kafkaVersion,
@@ -11,3 +11,13 @@ ThisBuild / libraryDependencies ++= Seq(
 )
 
 lazy val root = (project in file(".")).settings(name := "KafkaPerfTest")
+
+assembly / assemblyMergeStrategy := {
+  case m if m.toLowerCase.endsWith("manifest.mf")       => MergeStrategy.discard
+  case m if m.toLowerCase.matches("meta-inf.*\\.sf$")   => MergeStrategy.discard
+  case m if m.toLowerCase.matches("version.conf")       => MergeStrategy.discard
+  case "module-info.class"                              => MergeStrategy.first
+  case "reference.conf"                                 => MergeStrategy.concat
+  case x: String if x.contains("UnusedStubClass.class") => MergeStrategy.first
+  case _                                                => MergeStrategy.first
+}
--- KafkaPerfTest/src/main/scala/ru/example/kafka/KafkaPerfTest.scala
+++ KafkaPerfTest/src/main/scala/ru/example/kafka/KafkaPerfTest.scala
@@ -1,41 +1,47 @@
 package ru.example.kafka
 
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
-import org.apache.kafka.common.serialization.StringSerializer
 import java.time.Duration
 import java.util.Properties
 
 object KafkaPerfTest {
   def main(args: Array[String]): Unit = {
-    if (args.length != 3) {
-      println("Usage: KafkaPerfTest brokers topic messageCount")
+    if (args.length != 5) {
+      println("Usage: KafkaPerfTest brokers topic messageCount messageSize")
       sys.exit(-1)
     }
 
     val brokers      = args(0)
     val topic        = args(1)
-    val messageCount = args(2).toInt
+    val messageCount = args(2).toLong
+    val messageSize  = args(3).toInt
+    val batchSize    = args(4).toInt
 
     // Создаём Producer
     val props = new Properties()
     props.put("bootstrap.servers", brokers)
-    val producer = new KafkaProducer(props, new StringSerializer, new StringSerializer)
+    props.put("key.serializer", "org.apache.kafka.common.serialization.LongSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+    props.put("batch.size", batchSize)
+    val producer = new KafkaProducer[String, Array[Byte]](props)
+
+    // Создаём сообщение, которое будем отправлять
+    val value = Array.fill[Byte](messageSize)(0)
 
     // Отправляем в Kafka
     val start = System.nanoTime
-
-    (0 until messageCount) foreach { _ =>
-      producer.send(new ProducerRecord(topic, "", "1"))
+    (0L until messageCount) foreach { _ =>
+      producer.send(new ProducerRecord(topic, value))
     }
-
     val end = System.nanoTime
+
+    // Выводим результат
     println(
-      s"Published $messageCount messages in ${Duration.ofNanos(end - start).toMillis} ms"
+      s"Published $messageCount messages with size $messageSize by batch $batchSize in ${Duration.ofNanos(end - start).toMillis} ms"
     )
 
     // Закрываем и выходим
     producer.close()
     sys.exit(0)
-
   }
 }
